05035d5ccf5fe30ba52be752bae915c65b37f8ce
jaroslaw91 <jaroslawkucharski91@gmail.com>
2021-03-13 18:39:18 +0100

    Added i18n files

---
+++ frontend/src/services/i18n/i18-js.tsx
@@ -0,0 +1,17 @@
+import * as Localization from 'expo-localization';
+import i18n from 'i18n-js';
+import messeges_en from './en.json'
+import messeges_pl from './pl.json'
+
+export default function language(message: any) {
+    i18n.translations = {
+        en: messeges_en,
+        pl: messeges_pl,
+    };
+
+    i18n.locale = Localization.locale;
+
+    i18n.fallbacks = true;
+
+    return i18n.t(message)
+}
