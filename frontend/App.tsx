36f49830c51c3370b4068f09586356f83ddd5f34
jaroslaw91 <jaroslawkucharski91@gmail.com>
2021-03-15 11:39:10 +0100

    Code fixed

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, View } from 'react-native'
-import { TextComponent } from './src/components/TextComponent'
+import { TextComponent } from 'components/ui/Text/'
 
 const App = () => {
   return (
