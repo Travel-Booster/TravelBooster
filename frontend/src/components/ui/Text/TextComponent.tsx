f6cbd9641306d5e8140a2733c613d7d422652bae
jaroslaw91 <jaroslawkucharski91@gmail.com>
2021-03-15 12:02:18 +0100

    Code fixed

--- frontend/src/components/ui/Text/TextComponent.tsx
+++ frontend/src/components/ui/Text/TextComponent.tsx
@@ -1,11 +1,18 @@
 import React, { memo } from 'react'
 import { Text } from 'react-native'
-import { message } from "helpers/Translation"
+import { message } from "helpers/getTranslation"
 
 type TextComponentProps = {
   text: string
 }
 
+/**
+ * @param {string}  p1 - A string param.
+ * @param {string=} p2 - An optional param (Closure syntax)
+ * @param {string} [p3] - Another optional param (JSDoc syntax).
+ * @param {string} [p4="test"] - An optional param with a default value
+ * @return {string} This is the result
+ */
 export const TextComponent = memo<TextComponentProps>(({ text }) => {
   return <Text>{message(text)}</Text>
 })
