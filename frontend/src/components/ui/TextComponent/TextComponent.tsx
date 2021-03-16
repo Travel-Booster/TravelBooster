bc36cf6f222cc0c0a29f363a6c545743a434402c
ZychGrzegorz <grzegorzzych95@gmail.com>
2021-03-16 02:02:48 +0100

    config done

--- frontend/src/components/ui/TextComponent/TextComponent.tsx
+++ frontend/src/components/ui/TextComponent/TextComponent.tsx
@@ -1,11 +1,17 @@
 import React, { memo } from 'react'
 import { Text } from 'react-native'
-import { message } from "helpers/getTranslation"
+import { message } from 'helpers/getTranslation'
 
 type TextComponentProps = {
   text: string
 }
 
+/**
+ * function that returns a text
+ * @property {Function} TextComponent - Function
+ * @param text - Text for component
+ * @returns - Text component
+ */
 const TextComponent = memo<TextComponentProps>(({ text }) => {
   return <Text>{message(text)}</Text>
 })
--- frontend/src/helpers/getTranslation.tsx
+++ frontend/src/helpers/getTranslation.tsx
@@ -3,23 +3,31 @@ import i18n from 'i18n-js'
 import messeges_en from 'services/i18n/en.json'
 import messeges_pl from 'services/i18n/pl.json'
 
-//default language is taken from phone settings
+/**
+ * Default language is taken from phone settings
+ */
 let language = Localization.locale
 
-//function that changes the language
+/**
+ * Function that changes the language
+ * @param newLang - New language
+ */
 export const setLanguage = (newLang: string) => {
   language = newLang
 }
 
 // setLanguage('en-US')
+
+/**
+ * @param message - Message text parameter
+ * @returns - Returns text in a choosen language
+ */
 export const message = (message: string) => {
   i18n.translations = {
     en: messeges_en,
     pl: messeges_pl,
   }
-
   i18n.locale = language
-
   i18n.fallbacks = true
   return i18n.t(message)
 }
