b46dd325d68426cdbaa2059710368747d841e67c
ZychGrzegorz <grzegorzzych95@gmail.com>
2021-03-17 15:16:04 +0100

    pre nodemon

--- frontend/src/helpers/getTranslation.tsx
+++ frontend/src/helpers/getTranslation.tsx
@@ -3,29 +3,27 @@ import i18n from 'i18n-js'
 import messeges_en from 'services/i18n/en.json'
 import messeges_pl from 'services/i18n/pl.json'
 /**
- * Jira client library
- *
+ * Default language is taken from phone settings
  * @example
- * const jira = new Jira({
- *   host: 'yourhost',
- *   user: 'jhn@doe.com',
- *   token: 'secret',
- * })
- *
- * const projects = await jira.projects()
- *
+ * let language = Localization.locale
  * @see  https://developer.atlassian.com/cloud/jira/platform/rest/v3
  * @todo  implement all methods
  */
-//default language is taken from phone settings
 let language = Localization.locale
 
-//function that changes the language
+/**
+ * function that changes the language
+ * @param newLang newlang
+ */
 export const setLanguage = (newLang: string) => {
   language = newLang
 }
 
-// setLanguage('en-US')
+/**
+ * sth here
+ * @param message msg
+ * @returns nothing
+ */
 export const message = (message: string) => {
   i18n.translations = {
     en: messeges_en,
