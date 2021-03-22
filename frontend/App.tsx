4591bc7c5cdb622811f937c4fe85c834be5f86be
jaroslaw91 <jaroslawkucharski91@gmail.com>
2021-03-22 21:59:54 +0100

    Added new ButtonComponent to App

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, View } from 'react-native'
 import TextComponent from 'components/ui/TextComponent'
+import ButtonComponent from 'components/ui/ButtonComponent'
 
 const App = () => {
   return (
     <View style={styles.container}>
       <TextComponent text='hello' />
+      <ButtonComponent>
+        Click me
+      </ButtonComponent>
       <StatusBar style='auto' />
     </View>
   )
