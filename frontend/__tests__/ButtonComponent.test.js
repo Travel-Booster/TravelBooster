b8a026d767242b67a0826d9c4e1ee787a47189f7
jaroslaw91 <jaroslawkucharski91@gmail.com>
2021-03-23 21:05:19 +0100

    Test updated & created test for Button component

---
+++ frontend/__tests__/ButtonComponent.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import ButtonComponent from 'components/ui/ButtonComponent'
+
+const props = {
+    text: 'button',
+    action: () => console.log('test'),
+    color: 'error',
+    width: 'full'
+}
+
+const component = <ButtonComponent {...props} />
+let wrapper = renderer.create(component).toJSON()
+
+it('<TextComponent /> renders', () => {
+    expect(wrapper).toBeTruthy()
+})
+    
+it('<TextComponent /> test snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+})
