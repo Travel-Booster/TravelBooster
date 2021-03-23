3371a254778e7e241eb21f6cf526640e564559d4
jaroslaw91 <jaroslawkucharski91@gmail.com>
2021-03-23 16:04:50 +0100

    App fixed

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,18 +1,33 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
+import AppLoading from 'expo-app-loading'
 import { StyleSheet, View } from 'react-native'
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_600SemiBold,
+  Montserrat_800ExtraBold
+} from '@expo-google-fonts/montserrat'
 import TextComponent from 'components/ui/TextComponent'
 import ButtonComponent from 'components/ui/ButtonComponent'
 
 const App = () => {
+  let [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_600SemiBold,
+    Montserrat_800ExtraBold
+  })
+
   return (
-    <View style={styles.container}>
-      <TextComponent text='hello' />
-      <ButtonComponent action={() => alert('button test')}>
-        <TextComponent text='button' />
-      </ButtonComponent>
-      <StatusBar style='auto' />
-    </View>
+    !fontsLoaded
+      ? <AppLoading />
+      : <View style={styles.container}>
+          <TextComponent text='hello' />
+          <ButtonComponent 
+            action={() => alert('button test')}
+            text='button' />
+          <StatusBar style='auto' />
+        </View>
   )
 }
 
@@ -21,7 +36,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#ffffff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 })
 
