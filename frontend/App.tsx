388587b4ed303edb7e50ab232a0d031ada2e6535
jaroslaw91 <jaroslawkucharski91@gmail.com>
2021-03-28 14:25:56 +0200

    Formatted config files

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,95 +1,90 @@
-import React from 'react'
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, View } from 'react-native'
-import { useFonts } from 'expo-font'
-import colors from 'styles/colors'
-import paddings from 'styles/paddings'
-import AppLoading from 'expo-app-loading'
-import TextComponent from 'components/ui/TextComponent'
 import ButtonComponent from 'components/ui/ButtonComponent'
 import DividerComponent from 'components/ui/DividerComponent'
+import TextComponent from 'components/ui/TextComponent'
+import AppLoading from 'expo-app-loading'
+import { useFonts } from 'expo-font'
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import colors from 'styles/colors'
+import paddings from 'styles/paddings'
 
 const App = () => {
-  let [fontsLoaded] = useFonts({
-    'montserrat-regular': require('./assets/fonts/Montserrat-Regular.ttf'),
-    'montserrat-semi-bold': require('./assets/fonts/Montserrat-SemiBold.ttf'),
-    'montserrat-extra-bold': require('./assets/fonts/Montserrat-ExtraBold.ttf')
-  })
+	const [fontsLoaded] = useFonts({
+		'montserrat-regular': require('./assets/fonts/Montserrat-Regular.ttf'),
+		'montserrat-semi-bold': require('./assets/fonts/Montserrat-SemiBold.ttf'),
+		'montserrat-extra-bold': require('./assets/fonts/Montserrat-ExtraBold.ttf'),
+	})
 
-  return (
-    !fontsLoaded
-      ? <AppLoading />
-      : <View style={styles.container}>
-          <TextComponent 
-            text='hello'
-            variant='heading' />
+	return !fontsLoaded ? (
+		<AppLoading />
+	) : (
+		<View style={styles.container}>
+			<TextComponent
+				text="hello"
+				variant="heading"
+				variant="heading"
+				variant="heading"
+			/>
 
-          <DividerComponent 
-            size='big' />
+			<DividerComponent size="big" />
 
-          <TextComponent 
-            text='hello'
-            variant='sub' />
+			<TextComponent text="hello" variant="sub" />
 
-          <DividerComponent />
+			<DividerComponent />
 
-          <TextComponent 
-            text='hello'
-            variant='title' />
+			<TextComponent text="hello" variant="title" />
 
-          <DividerComponent 
-            size='tiny' />
+			<DividerComponent size="tiny" />
 
-          <TextComponent 
-            text='hello'
-            variant='description' />
+			<TextComponent text="hello" variant="description" />
 
-          <DividerComponent 
-            size='small' />
+			<DividerComponent size="small" />
 
-          <TextComponent text='hello' />
+			<TextComponent text="hello" />
 
-          <DividerComponent />
+			<DividerComponent />
 
-          <ButtonComponent 
-            action={() => alert('button test')}
-            text='button' />
+			<ButtonComponent action={() => alert('button test')} text="button" />
 
-          <DividerComponent />
+			<DividerComponent />
 
-          <ButtonComponent 
-            action={() => alert('button test')}
-            text='button'
-            color='error' />
+			<ButtonComponent
+				action={() => alert('button test')}
+				text="button"
+				color="error"
+			/>
 
-          <DividerComponent />
+			<DividerComponent />
 
-          <ButtonComponent 
-            action={() => alert('button test')}
-            text='button'
-            width='full' />
+			<ButtonComponent
+				action={() => alert('button test')}
+				text="button"
+				width="full"
+			/>
 
-          <DividerComponent />
+			<DividerComponent />
 
-          <ButtonComponent 
-            action={() => alert('button test')}
-            text='button'
-            color='error'
-            width='full' />
-          <StatusBar style='auto' />
-        </View>
-  )
+			<ButtonComponent
+				action={() => alert('button test')}
+				text="button"
+				color="error"
+				width="full"
+			/>
+			<StatusBar style="auto" />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: paddings.BIG,
-    backgroundColor: colors.LIGHT_MAIN,
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontFamily: 'Regular'
-  }
+	container: {
+		flex: 1,
+		padding: paddings.BIG,
+		backgroundColor: colors.LIGHT_MAIN,
+		alignItems: 'center',
+		justifyContent: 'center',
+		fontFamily: 'Regular',
+	},
 })
 
 export default App
